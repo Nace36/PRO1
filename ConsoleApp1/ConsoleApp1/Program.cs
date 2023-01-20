e8d24954507badbb94dbc40976889d5f7d3fceb3
Nace Kodermac <nacko.kodermacko@gmail.com>
2023-01-20 09:52:40 +0100

    Vaja

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Stari donald farmo ima");
+            Console.WriteLine("IAIAO");
+            Console.WriteLine("Na tej farmi osla dva");
+            Console.WriteLine("IAIAO");
+        }
+    }
+}
